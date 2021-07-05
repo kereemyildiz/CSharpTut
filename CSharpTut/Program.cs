2d0cb0e865a91f59f4de84c3d39feba621aa01da
kereemyildiz <a.keremyildiz99@gmail.com>
2021-07-05 15:01:38 +0300

    virtual-abstract methods

--- CSharpTut/Program.cs
+++ CSharpTut/Program.cs
@@ -10,10 +10,30 @@ namespace CSharpTut
     {
         static void Main(string[] args)
         {
-            Warrior maximus = new Warrior("Maximus", 1000, 120, 40); // name,health,attack value, block value
-            Warrior bob = new Warrior("Bob", 1000, 120, 40);
-            Battle.StartFight(maximus, bob);
+            Shape[] shapes = {new Circle(5),
+            new Rectangle(4,5)};
+
+            foreach (Shape s in shapes)
+            {
+                s.GetInfo();
+                Console.WriteLine($"{s.Name} Area : {s.Area()}");
+                Circle testCirc = s as Circle;
+                if (testCirc == null)
+                {
+                    Console.WriteLine("This is not a circle");
+                }
+                if (s is Circle)
+                {
+                    Console.WriteLine("This is not a Rectangle");
+                }
+
+                object circ1 = new Circle(4);
+                Circle circ2 = (Circle)circ1;
+                Console.WriteLine("The {0} Area is {1:f2}", circ2.Name, circ2.Area());
+                Console.WriteLine();
+
+            }
             Console.ReadLine();
-        }
+        }  
     }
 }
