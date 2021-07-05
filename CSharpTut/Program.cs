64c29202669cb0dc90f09a64a028e8d15681bd71
kereemyildiz <a.keremyildiz99@gmail.com>
2021-07-05 15:31:26 +0300

    implement interface

--- CSharpTut/Program.cs
+++ CSharpTut/Program.cs
@@ -10,29 +10,22 @@ namespace CSharpTut
     {
         static void Main(string[] args)
         {
-            Shape[] shapes = {new Circle(5),
-            new Rectangle(4,5)};
-
-            foreach (Shape s in shapes)
+            Vehicle buick = new Vehicle("Buick", 4, 160);
+            if (buick is IDrivable)
             {
-                s.GetInfo();
-                Console.WriteLine($"{s.Name} Area : {s.Area()}");
-                Circle testCirc = s as Circle;
-                if (testCirc == null)
-                {
-                    Console.WriteLine("This is not a circle");
-                }
-                if (s is Circle)
-                {
-                    Console.WriteLine("This is not a Rectangle");
-                }
-
-                object circ1 = new Circle(4);
-                Circle circ2 = (Circle)circ1;
-                Console.WriteLine("The {0} Area is {1:f2}", circ2.Name, circ2.Area());
-                Console.WriteLine();
-
+                buick.Move();
+                buick.Stop();
             }
+            else
+            {
+                Console.WriteLine("The {0} cant be driven", buick.Brand);
+            }
+
+            IElectornicDevice TV = TVRemote.GetDevice();
+            PowerButton powBut = new PowerButton(TV);
+            powBut.Execute();
+            powBut.Undo();
+
             Console.ReadLine();
         }  
     }
---
+++ CSharpTut/TVRemote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut
+{
+    class TVRemote
+    {
+        public static IElectornicDevice GetDevice()
+        {
+            return new Television();
+        }
+    }
+}
