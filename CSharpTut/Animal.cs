a99b8757e09b8309934cb9c4cbc9125697637063
kereemyildiz <a.keremyildiz99@gmail.com>
2021-07-05 10:45:29 +0300

    Add project files.

---
+++ CSharpTut/Animal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpTut
+{
+    class Animal
+    {
+        public string name;
+        public string sound;
+        public const string SHELTER = "Shelter";
+
+        public readonly int idNum;
+
+
+        public Animal() :
+            this("No Name", "No Sound"){ }
+
+        public Animal(string name) :
+            this(name, "No Sound"){ }
+
+        public Animal(string name,string sound)
+        {
+            SetName(name);
+            Sound = sound;
+            NumOfAnimals = 1;
+            Random rnd = new Random();
+            idNum = rnd.Next(1, 1000);
+        }
+
+        public void SetName(string name)
+        {
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+
+            }
+            else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name cannot contain numbers");
+            }
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");
+                }
+                sound = value;
+            }
+        }
+
+        public string Owner { get; set; } = "No Owner";
+
+        public static int numOfAnimals = 0;
+
+        public static int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+
+
+
+    }
+}
---
+++ CSharpTut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Animal();
+
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+
+            Console.WriteLine("The cat is named {0} and says {1}", cat.GetName(), cat.Sound);
+
+            cat.Owner = "Kerem";
+
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+            Console.WriteLine("{0} shelter id is {1}", cat.GetName(), cat.idNum);
+            Console.WriteLine("# of Animals {0}", Animal.NumOfAnimals);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
